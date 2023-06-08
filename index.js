e7cf36d1170a57792ced46f111e5c1f2d32f4cfe
Simon <simon@hessner.de>
2023-06-08 01:16:25 +0100

    Exercise 3.4

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ app.get('/', (_, response) => {
     response.send("<h1>Phonebook</h1>")
 })
 
+app.get('/info', (_, response) => {
+    const number = persons.length
+
+    const msg = `Phonebook has info for ${number} people. <br /> <br />${(new Date())}`
+    response.send(msg)
+})
+
 app.get('/api/persons', (_, response) => {
     response.json(persons)
 })
@@ -45,11 +52,11 @@ app.get('/api/persons/:id', (request, response) => {
     response.json(person)
 })
 
-app.get('/info', (_, response) => {
-    const number = persons.length
-
-    const msg = `Phonebook has info for ${number} people. <br /> <br />${(new Date())}`
-    response.send(msg)
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    // No check if ID exists. If it has already been deleted, we consider the operation successful.
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
 })
 
 PORT = 3001
