c669e597e86f8be85a32a190e5e730347731e110
Simon <simon@hessner.de>
2023-06-10 16:16:00 +0100

    Excercise 3.12

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstackopen:${password}@cluster0.gbaymfj.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+    console.log("Phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.phone)
+        })
+        mongoose.connection.close()
+    })
+} else if (process.argv.length == 5) {
+    const person = new Person({
+        name: process.argv[3],
+        phone: process.argv[4],
+      })
+
+      person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+      })
+      
+} else {
+    console.log("Either give 3 arguments to show or 5 to add a person")
+    mongoose.connection.close()
+}
